b36becc73c61725a6fbd7ccd3677766dce0aeb4d
Akifumi KAMADA <akifumi3@gmail.com>
2011-05-10 00:39:23 +0000

    test xp

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World,\nthis is test 03.\n');
+  res.end('Hello World,\nthis is test 03.\ntest----!\n');
 }).listen(process.env.C9_PORT, "0.0.0.0");
 
 console.log('Server running at http://cloud9-test03.akifumi3_1.c9.io');
